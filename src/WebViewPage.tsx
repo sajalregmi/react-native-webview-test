4628e6e7d1b8bd7ac5d85ca1abf8399745306242
sajal <sajalregmi4@gmail.com>
2024-07-30 21:13:14 -0700

    scroll issue fixed

--- src/WebViewPage.tsx
+++ src/WebViewPage.tsx
@@ -3,15 +3,24 @@ import { View, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const WebViewPage = () => {
+  const webViewRef = React.useRef(null);
   return (
     <View style={styles.container}>
       <WebView 
-        source={{ uri: 'http://localhost:8080/web/scrollTest.html' }} 
+              ref={webViewRef}
+        source={{ uri: 'http://192.168.0.248:8080/web/scrollTest3.html' }} 
         style={styles.webview}
         javaScriptEnabled={true} 
+        cacheEnabled={false}
         injectedJavaScript={`const style = document.createElement('style');
                                style.innerHTML = 'body { overflow-x: hidden; }';
                                document.head.appendChild(style);`}
+                                       onLoadEnd={() => {
+          // Clear the cache after loading the page
+          if (webViewRef.current) {
+            webViewRef.current.clearCache(true);
+          }
+        }}
       />
     </View>
   );
